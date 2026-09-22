8ba4096d241de125cebca7c4c75d65ca7dcc58c3
renderprueba09-prog <renderprueba09@gmail.com>
2026-09-22 07:50:56 -0500

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import app  # Cambia 'app' por el nombre de tu archivo principal (ej. si tu archivo se llama main.py, sería 'from main import app')
+
+if __name__ == "__main__":
+    app.run()
